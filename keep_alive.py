e3fa7fcbc8ca218df9c316e73a47e3617f9f8957
novterss <Meliodazzzzeiei@gmail.com>
2025-12-25 00:47:05 +0700

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+import os
 
 app = Flask('')
 
@@ -8,7 +9,8 @@ def home():
     return "I'm alive"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+  # แก้ตรงนี้: ให้ดึง PORT จาก Environment ของเครื่องเซิฟเวอร์ ถ้าไม่มีค่อยใช้ 8080
+  app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
 
 def keep_alive():
     t = Thread(target=run)
